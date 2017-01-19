5f1852c390889d0e4aa6377be5cc9180a6f3abd8
Andrew Duerig <aqd5396@psu.edu>
2017-01-19 06:37:05 -0500

    Added Renderer.py and set up framework.

---
+++ Renderer.py
@@ -0,0 +1,17 @@
+import math
+import random
+import graphics
+
+class Renderer:
+
+    def __init__(self):
+        pass
+
+
+
+    # Probably move this to a GameRenderer.py class
+    def window_setup(self):
+        if __name__ == '__main__':
+            win = GraphWin("Chess", width, height)
+        else:
+            return "Not the main thread/proccess!"
