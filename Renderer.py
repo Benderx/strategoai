434a2243c2cd0b94350c7538dbaab7b80ddab075
Andrew Duerig <aqd5396@psu.edu>
2017-01-19 06:57:02 -0500

    Minor spelling and import fixes.

--- Renderer.py
+++ Renderer.py
@@ -4,25 +4,27 @@ import graphics
 
 class Renderer:
 
-    def __init__(self, b, l = 15):
+    def __init__(self, b, l = 15, o = 5):
         self.board = b
         self.box_length = l
-        piece_arr = []
+        self.offset = o
+        self.piece_arr = []
         self.win = None
 
 
     def draw_board(self):
-        for i in range(len(self.board_):
+        for i in range(len(self.board)):
             for j in range(len(self.board[i])):
-                piece = Text(Point(self.box_length * i + offset + (self.box_ength / 2),
-                                      self.box_length * j + offset + (self.box_length / 2)), "NO")
-                piece_arr.append(piece)
-                piece.draw(win)
+                piece = graphics.Text(graphics.Point(self.box_length * i + self.offset + (self.box_length / 2),
+                                      self.box_length * j + self.offset + (self.box_length / 2)), "NO")
+                self.piece_arr.append(piece)
+                piece.draw(self.win)
 
 
     # Takes in width and height and sets up the Graphical Window.
     def window_setup(self, width, height):
         if __name__ == '__main__':
-            self.win = graphics.GraphWin("Chess", width, height)
+            self.win = graphics.GraphWin("Stratego!", width, height)
+            self.win.setBackground("tan2")
         else:
             return "Not the main thread/proccess!"
