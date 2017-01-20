4a14a50de781a27d6cdc1b176b1e8316febe48d3
Andrew Duerig <aqd5396@psu.edu>
2017-01-20 07:44:33 -0500

    Finished first design of database for games to be tracked. Does not include room for probability storage yet.

--- driver.py
+++ driver.py
@@ -2,10 +2,15 @@ import Renderer as r
 import GameEngine as g
 import time
 import sqlite3
+import os
 
 
 # humans = 0, 1, 2
-def play_game(engine, humans = 1, gui = False, renderer = None):
+def play_game(engine, humans = 1, db_conn = None, gui = False, renderer = None):
+    tracking = True
+    if db_conn == None:
+        tracking = False
+
     players = []
     if humans == 0:
         players.append(YOURAIHERE(0, ARGS))
@@ -19,8 +24,16 @@ def play_game(engine, humans = 1, gui = False, renderer = None):
     else:
         raise Exception('This is a two player game, you listed more than 2 humans, or less than 0.')
 
+
     engine.board_setup()
-    renderer.draw_board()
+    if gui:
+        renderer.draw_board()
+    else:
+        engine.print_board()
+
+    if tracking:
+        pass
+        # record_board(engine)
 
     playing = True
     turn = 0
@@ -43,15 +56,52 @@ def play_game(engine, humans = 1, gui = False, renderer = None):
         turn = 1 - turn
 
 
+# Takes in database name and if you want to overwrite current, or add to it. Probably change in future for streamlined data creation
+# Returns sqlite3 db connection
+def init_db(dbname = 'test.db', overwrite = True):
+    exist = False
+    if os.fileexists(dbpath)
+        exist = True
+
+    if overwrite and exist:
+        os.remove(dbpath)
+    
+    conn = sqlite3.connect(dbpath)
+
+    if overwrite or exist == False:
+        create_game_sql = """
+                        CREATE TABLE Game (
+                       ID INT PRIMARY KEY AUTOINCREMENT,
+                       WINNER INT);
+                    """
+        conn.execute(create_game_sql)
+
+        # How should we store the board?
+        create_state_sql = """
+                        CREATE TABLE State (
+                       ID INT PRIMARY KEY AUTOINCREMENT,
+                       GAME_ID INT FOREIGN KEY NOT NULL,
+                       BOARD CHAR(500) NOT NULL);
+                    """
+        conn.execute(create_state_sql)
+    return conn
+
+
 def main():
     engine = g.GameEngine()
     re = r.Renderer(engine.get_board())
     re.window_setup(500, 500)
 
-    play_game(engine, 2, True, re)
-
+    db_conn = init_db('games.db', True)
+    play_game(engine, 2, db_conn, True, re)
 
 main()
+
+
+
+
+
+
 
 # time.sleep(2)
 
