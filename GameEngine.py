2a4a2e99b35026ba1019834e0dc19b44fbd08a1e
Andrew Duerig <aqd5396@psu.edu>
2017-01-24 21:45:44 -0500

    Further c progress

--- GameEngine.py
+++ GameEngine.py
@@ -1,7 +1,11 @@
 import math
 import random
 import inspect
+import numpy
 from copy import deepcopy
+from libc.stdlib cimport malloc, free
+import c_bindings.get_moves as c_moves
+
 
 class Piece:
     # 0: Flag
@@ -16,17 +20,18 @@ class Piece:
         self.value = v
         self.abbrev = a
         self.visible = False
+        self.movement = self.get_movement(v)
 
     def __str__(self):
         return str(self.value)
     __repr__ = __str__
 
     # returns how many spaces the piece may move.
-    def get_movement(self):
-        if self.value > 0 and self.value < 9 or self.value == 11:
+    def get_movement(self, val):
+        if val > 0 and val < 9 or val == 11:
             return 1
 
-        elif self.value == 9:
+        elif val == 9:
             return 100
 
         else:
@@ -57,37 +62,54 @@ class Piece:
 class GameEngine:
     def __init__(self, n):
         self.size = n
-        self.board = [[None for x in range(0, n)] for y in range(0, n)]
+        self.board = numpy.zeros(0, n * n)
+        self.owner = numpy.zeros(0, n * n)
+        self.visibility = numpy.zeros(0, n * n)
         self.flags = [[-1, -1], [-1, -1]]
         self.move_history = []
 
+
+    def convert_to_numpy_1D(b, b2):
+        for x in range(self.size):
+            for y in range(self.size):
+                b2[x + (x * y)] = b[x][y]
+
+
+    def get_2D_array(b):
+        board_temp = [[0 for x in range(0, self.size)] for x in range(0, self.size)]
+        for x in range(self.size):
+            for y in range(self.size):
+                board_temp[x][y] = b[x + (x * y)]
+        return board_temp
+
+
     def board_setup(self):
-        for x in range(len(self.board)):
-            for y in range(len(self.board)):
-                self.board[x][y] = 0
+        owner_temp = [[0 for x in range(0, self.size)] for x in range(0, self.size)]
+        visibility_temp = [[0 for x in range(0, self.size)] for x in range(0, self.size)]
+        board_temp = [[0 for x in range(0, self.size)] for x in range(0, self.size)]
 
         # Rivers
         if self.size == 10:
-            self.board[2][4] = Piece(None, -1, 'K')
-            self.board[2][5] = Piece(None, -1, 'K')
-            self.board[3][4] = Piece(None, -1, 'K')
-            self.board[3][5] = Piece(None, -1, 'K')
+            board_temp[2][4] = Piece(None, -1, 'K')
+            board_temp[2][5] = Piece(None, -1, 'K')
+            board_temp[3][4] = Piece(None, -1, 'K')
+            board_temp[3][5] = Piece(None, -1, 'K')
 
-            self.board[6][4] = Piece(None, -1, 'K')
-            self.board[6][5] = Piece(None, -1, 'K')
-            self.board[7][4] = Piece(None, -1, 'K')
-            self.board[7][5] = Piece(None, -1, 'K')
+            board_temp[6][4] = Piece(None, -1, 'K')
+            board_temp[6][5] = Piece(None, -1, 'K')
+            board_temp[7][4] = Piece(None, -1, 'K')
+            board_temp[7][5] = Piece(None, -1, 'K')
         elif self.size == 6:
-            self.board[2][2] = Piece(None, -1, 'K')
-            self.board[2][3] = Piece(None, -1, 'K')
+            board_temp[2][2] = Piece(None, -1, 'K')
+            board_temp[2][3] = Piece(None, -1, 'K')
 
         for i in range(0, 2):
             if self.size == 10:
                 rows = 4
-                starting_pieces = [[0, 'Flag F', 1], [10, 'Bomb B', 6], [11, 'Spy Y', 1], [9, 'Scout S', 8], [8, 'Miner R', 5], [7, 'Sergeant T', 4], [6, 'Lieutenent L', 4], [5, 'Captain C', 4], [4, 'Major J', 3], [3, 'Colonel O', 2], [2, 'General G', 1], [1, 'Marshall M', 1]]
+                starting_pieces = [[12, 'Flag F', 1], [10, 'Bomb B', 6], [11, 'Spy Y', 1], [9, 'Scout S', 8], [8, 'Miner R', 5], [7, 'Sergeant T', 4], [6, 'Lieutenent L', 4], [5, 'Captain C', 4], [4, 'Major J', 3], [3, 'Colonel O', 2], [2, 'General G', 1], [1, 'Marshall M', 1]]
             elif self.size == 6:
                 rows = 2
-                starting_pieces = [[0, 'Flag F', 1], [10, 'Bomb B', 1], [11, 'Spy Y', 1], [9, 'Scout S', 1], [8, 'Miner R', 1], [7, 'Sergeant T', 1], [6, 'Lieutenent L', 1], [5, 'Captain C', 1], [4, 'Major J', 1], [3, 'Colonel O', 1], [2, 'General G', 1], [1, 'Marshall M', 1]]
+                starting_pieces = [[12, 'Flag F', 1], [10, 'Bomb B', 1], [11, 'Spy Y', 1], [9, 'Scout S', 1], [8, 'Miner R', 1], [7, 'Sergeant T', 1], [6, 'Lieutenent L', 1], [5, 'Captain C', 1], [4, 'Major J', 1], [3, 'Colonel O', 1], [2, 'General G', 1], [1, 'Marshall M', 1]]
 
 
             starting_locations = []
@@ -97,12 +119,14 @@ class GameEngine:
             for x in range(0, self.size):
                 for y in range(0 + i*(self.size-rows), rows + i*(self.size-rows)):
                     if y == 0 and x == randy and i == 0:
-                        self.board[x][y] = Piece(0, 0, 'F')
+                        owner_temp[x][y] = 0
+                        board_temp[x][y] = 12
                         self.flags[0] = [x, y]
                         starting_pieces.pop(0)
                         continue
                     if y == rows + i*(self.size-rows)-1 and x == randy2 and i == 1:
-                        self.board[x][y] = Piece(1, 0, 'F')
+                        owner_temp[x][y] = 1
+                        board_temp[x][y] = 12
                         self.flags[1] = [x, y]
                         starting_pieces.pop(0)
                         continue
@@ -112,21 +136,26 @@ class GameEngine:
                 r1 = int(random.random()*(len(starting_pieces)))
                 r2 = int(random.random()*(len(starting_locations)))
 
-                p = Piece(starting_locations[r2][2], starting_pieces[r1][0], starting_pieces[r1][1][-1])
-                self.board[starting_locations[r2][0]][starting_locations[r2][1]] = p
+                owner_temp[starting_locations[r2][0]][starting_locations[r2][1]] = starting_locations[r2][2]
+                board_temp[starting_locations[r2][0]][starting_locations[r2][1]] = starting_pieces[r1][0]
 
                 starting_locations.pop(r2)
                 starting_pieces[r1][2] -= 1
                 if starting_pieces[r1][2] == 0:
                     starting_pieces.pop(r1)
+        convert_to_numpy_1D(board_temp, self.visibility)
+        convert_to_numpy_1D(owner_temp, self.visibility)
+        convert_to_numpy_1D(visibility_temp, self.visibility)
 
     def print_board(self):
+        board_to_print = self.get_2D_array(self.board)
+        # owner_board = self.get_2D_array(self.owner)
         print()
         for x in range(self.size):
             arr_temp = []
             for y in range(self.size):
-                if self.board[y][x]:
-                    val = self.board[y][x].get_value()
+                if board_to_print[y][x]:
+                    val = board_to_print[y][x].get_value()
                     if val == 0:
                         name = 'F'
                     elif val == 10:
@@ -148,82 +177,78 @@ class GameEngine:
 
     # Takes in coord1 [x1, y1] and coord2 [x2, y2], and player = (0 or 1)
     # Assumes there is a piece ar coord1
-    def check_legal(self, coord1, coord2, player):
-        piece = self.board[coord1[0]][coord1[1]]
-        if piece.get_player() != player:
-            return False, 'That is not your piece'
+    # def check_legal(self, coord1, coord2, player):
+    #     piece = self.board[coord1[0]][coord1[1]]
+    #     if piece.get_player() != player:
+    #         return False, 'That is not your piece'
 
-        if self.board[coord2[0]][coord2[1]] != 0:
-            piece2 = self.board[coord2[0]][coord2[1]]
-            if piece2.get_value() == -1:
-                return False, 'Cannot move into a lake'
-            if player == piece2.get_player():
-                return False, 'You cant move into your own piece'
-        return True, 'legal move'
+    #     if self.board[coord2[0]][coord2[1]] != 0:
+    #         piece2 = self.board[coord2[0]][coord2[1]]
+    #         if piece2.get_value() == -1:
+    #             return False, 'Cannot move into a lake'
+    #         if player == piece2.get_player():
+    #             return False, 'You cant move into your own piece'
+    #     return True, 'legal move'
 
 
-    # Only for the renderer and the AI
-    def get_board(self):
-        return self.board
-
-    def get_visible_board(self, player):
-        visible_board = deepcopy(self.board)
-        piece_options = []
-        for x in range(len(self.board)):
-            for y in range(len(self.board)):
-                piece = visible_board[x][y]
-                if piece and piece.player != player and not piece.visible:
-                    visible_board[x][y].value = 0
-                    piece_options.append(piece.value)
-        return visible_board, piece_options
+    # def get_visible_board(self, player):
+    #     visible_board = deepcopy(self.board)
+    #     piece_options = []
+    #     for x in range(len(self.board)):
+    #         for y in range(len(self.board)):
+    #             piece = visible_board[x][y]
+    #             if piece and piece.player != player and not piece.visible:
+    #                 visible_board[x][y].value = 0
+    #                 piece_options.append(piece.value)
+    #     return visible_board, piece_options
 
 
 
     # Takes in 2 players and returns 0 for p1 winning and 1 for p2 winning, 2 for tie.
     # Also reveals.
-    def battle(self, p1, p2):
-        v1 = p1.get_value()
-        v2 = p2.get_value()
+    # def battle(self, p1, p2):
+    #     v1 = p1.get_value()
+    #     v2 = p2.get_value()
 
-        p1.reveal()
-        p2.reveal()
+    #     p1.reveal()
+    #     p2.reveal()
 
-        if v1 == 0:
-            return 1
+    #     if v1 == 0:
+    #         return 1
 
-        if v2 == 0:
-            return 0
+    #     if v2 == 0:
+    #         return 0
 
-        if v1 == 10:
-            if v2 == 8:
-                return 1
-            return 0
+    #     if v1 == 10:
+    #         if v2 == 8:
+    #             return 1
+    #         return 0
 
-        if v2 == 10:
-            if v1 == 8:
-                return 0
-            return 1
+    #     if v2 == 10:
+    #         if v1 == 8:
+    #             return 0
+    #         return 1
 
-        if v1 == 11:
-            if v2 == 1:
-                return 0
-            return 1
+    #     if v1 == 11:
+    #         if v2 == 1:
+    #             return 0
+    #         return 1
 
-        if v2 == 11:
-            if v1 == 1:
-                return 1
-            return 0
+    #     if v2 == 11:
+    #         if v1 == 1:
+    #             return 1
+    #         return 0
 
-        if v1 == v2:
-            return 2
+    #     if v1 == v2:
+    #         return 2
 
-        if v1 < v2:
-            return 0
-        if v1 > v2:
-            return 1
+    #     if v1 < v2:
+    #         return 0
+    #     if v1 > v2:
+    #         return 1
 
-        raise Exception('A case that was not thought of happened')
-        return False
+    #     raise Exception('A case that was not thought of happened')
+    #     return False
 
 
     # Takes in coord1 [x1, y1] and coord2 [x2, y2]
@@ -247,57 +272,59 @@ class GameEngine:
         return True
 
 
-    def legal_moves_for_piece(self, loc, player):
-        if not isinstance(self.board[loc[0]][loc[1]], Piece):
-            return []
-        moves = []
+    # def legal_moves_for_piece(self, loc, player):
+    #     if not isinstance(self.board[loc[0]][loc[1]], Piece):
+    #         return []
+    #     moves = []
 
-        speed = self.board[loc[0]][loc[1]].get_movement()
-        for i in range(1, speed+1):
-            if loc[0]+i < 0  or loc[0]+i > self.size-1:
-                break
-            if self.board[loc[0]+i][loc[1]] != 0:
-                moves.append((loc[0]+i, loc[1]))
-                break
-            moves.append((loc[0]+i, loc[1]))
+    #     speed = self.board[loc[0]][loc[1]].movement
+    #     for i in range(1, speed+1):
+    #         if loc[0]+i < 0  or loc[0]+i > self.size-1:
+    #             break
+    #         if self.board[loc[0]+i][loc[1]] != 0:
+    #             moves.append((loc[0]+i, loc[1]))
+    #             break
+    #         moves.append((loc[0]+i, loc[1]))
 
-        for i in range(1, speed+1):
-            if loc[0]-i < 0  or loc[0]-i > self.size-1:
-                break
-            if self.board[loc[0]-i][loc[1]] != 0:
-                moves.append((loc[0]-i, loc[1]))
-                break
-            moves.append((loc[0]-i, loc[1]))
+    #     for i in range(1, speed+1):
+    #         if loc[0]-i < 0  or loc[0]-i > self.size-1:
+    #             break
+    #         if self.board[loc[0]-i][loc[1]] != 0:
+    #             moves.append((loc[0]-i, loc[1]))
+    #             break
+    #         moves.append((loc[0]-i, loc[1]))
 
-        for i in range(1, speed+1):
-            if loc[1]+i < 0  or loc[1]+i > self.size-1:
-                break
-            if self.board[loc[0]][loc[1]+i] != 0:
-                moves.append((loc[0], loc[1]+i))
-                break
-            moves.append((loc[0], loc[1]+i))
+    #     for i in range(1, speed+1):
+    #         if loc[1]+i < 0  or loc[1]+i > self.size-1:
+    #             break
+    #         if self.board[loc[0]][loc[1]+i] != 0:
+    #             moves.append((loc[0], loc[1]+i))
+    #             break
+    #         moves.append((loc[0], loc[1]+i))
 
-        for i in range(1, speed+1):
-            if loc[1]-i < 0  or loc[1]-i > self.size-1:
-                break
-            if self.board[loc[0]][loc[1]-i] != 0:
-                moves.append((loc[0], loc[1]-i))
-                break
-            moves.append((loc[0], loc[1]-i))
+    #     for i in range(1, speed+1):
+    #         if loc[1]-i < 0  or loc[1]-i > self.size-1:
+    #             break
+    #         if self.board[loc[0]][loc[1]-i] != 0:
+    #             moves.append((loc[0], loc[1]-i))
+    #             break
+    #         moves.append((loc[0], loc[1]-i))
 
         
-        final = []
-        for move in moves:
-            if self.check_legal(loc, move, player)[0]:
-                final.append((loc, move))
-        return final
+    #     final = []
+    #     for move in moves:
+    #         if self.check_legal(loc, move, player)[0]:
+    #             final.append((loc, move))
+    #     return final
 
 
     def all_legal_moves(self, player):
-        moves = []
-        for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                moves += self.legal_moves_for_piece([i,j], player)
+        return c_moves.all_legal_moves(player, self.board, self.owner)
+        # else:
+        #     moves = []
+        #     for i in range(len(self.board)):
+        #         for j in range(len(self.board[i])):
+        #             moves += self.legal_moves_for_piece([i,j], player)
         return moves
 
 
