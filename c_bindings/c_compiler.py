ecd41b023ba3ad5aaa2683041901bf9b0b48a7ef
Andrew Duerig <aqd5396@psu.edu>
2017-01-25 04:50:26 -0500

    Removed 2 more useless c files

--- c_bindings/c_compiler.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-# define the extension module
-s = Extension('spam', sources=['spammodule.c'])
-
-# run the setup
-setup(ext_modules=[s])
--- c_bindings/c_test_driver.py
+++
@@ -1,6 +0,0 @@
-import imp
-print(imp.find_module("spam"))
-
-
-import spam
-status = spam.system("ls -l")
